5a58c97994fae43034ced71e09fa3bd13127c19d
fahadmirza <fahadmirza80@yahoo.com>
2016-12-31 14:14:46 -0600

    Initial commit

---
+++ HW1/machine-learning-ex1/ex1/computeCostMulti.m
@@ -0,0 +1,25 @@
+function J = computeCostMulti(X, y, theta)
+%COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
+%   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+h = X * theta;
+error = h - y;
+err_sqr = error.^2;
+err_sqr_sum = sum(err_sqr);
+J = err_sqr_sum / (2 * m);
+
+
+% =========================================================================
+
+end
