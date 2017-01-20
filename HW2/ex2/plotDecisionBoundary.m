6bc8121f7a64bb6e2b8ee732e40f7317aa2d822d
fahadmirza <fahadmirza80@yahoo.com>
2017-01-20 02:00:41 -0600

    HW3 added!

--- HW2/ex2/plotDecisionBoundary.m
+++ HW2/ex2/plotDecisionBoundary.m
@@ -41,7 +41,7 @@ else
 
     % Plot z = 0
     % Notice you need to specify the range [0, 0]
-    contour(u, v, z, [0, 0], 'LineWidth', 2)
+    contour(u, v, z, [0 0], 'LineWidth', 2)
 end
 hold off
 
